4d9847908eecc5a4e5d2489cd2cdbd19d811f109
perry <perryhp2012@gmail.com>
2023-07-28 16:44:47 +0800

    polished some css but will probably overhaul the design in the future, looks like ppt slides rn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
+
+
 
 const Cover = () => {
   return (
-    <section className='title-page'>
+    <section id='title' className='title-page'>
       <div className='title-container title-container-content'>
         <div className='subtitle subtitle-content'>
           Hello! I'm
@@ -17,14 +20,46 @@ const Cover = () => {
           I'm a first year UBC student hoping to pursue a Bachelor's in Computer Science. I am currently learning React and have experience in React Native, Javascript, and Java. I love to game, discover new music, exercise, and occasionally read books.
         </div>
       </div>
-      <div className='image-container'>
-        <img src={require('./assets/capy-grass-removebg-preview.png')} alt='capybara eating grass'/>
+      <div class='right-container'>
+        <div class='speech bottom'>
+          Yum
+        </div>
+        <div className='image-container'>
+          <img src={require('./assets/capy-grass-removebg-preview.png')} alt='capybara eating grass'/>
+        </div>
       </div>
     </section>
   )
 }
 
 const Experience = () => {
+  window.addEventListener('load', (event) => {
+    let bars = document.querySelectorAll('.bar1, .bar2, .bar3');
+    let certainPage = document.querySelectorAll('.experience-page');
+
+    let options = {
+      root: null
+    }
+
+    let observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                bars.forEach(function(bar) {
+                  bar.style.backgroundColor = '#b8a59c';
+                })
+            } else {
+                bars.forEach(function(bar) {
+                  bar.style.backgroundColor = 'white';
+                })
+            }
+        });
+    }, options);
+
+    certainPage.forEach(function(div) {
+        observer.observe(div);
+    });
+  })
+  
   return (
     <section id='experience' className='experience-page'>
       <header className='header-container header-container-content '>
@@ -84,10 +119,15 @@ const Project = () => {
           name='PainterApp'
           description='Mobile app developed with React Native and Expo that lets the user draw pictures with a variety of tools'
         />
+        <ProjectItem 
+          name='PlaylistSorter (WIP)'
+          description='WIP: plan to create a fullstack application that takes a youtube playlist and have the ability to sort videos by genre, artist, song length, and date added through calling youtube API'
+        />
       </div>
     </section>
   )
 }
+
 const Item = ({ position, time, location}) => {
   return (
     <div className='item-design item-layout'>
@@ -120,26 +160,29 @@ const ProjectItem = ({ name, description }) => {
 }
 
 const SideNav = () => {
- 
+  const [navVisible, setNavVisible] = useState(false)
+
   const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementsByClassName("button")[0].classList.toggle("change")
+    setNavVisible(prev => !prev);
   }
 
   const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0px";
+    document.getElementsByClassName("button")[0].classList.toggle("change")
+    setNavVisible(prev => !prev);
   }
   
   return (
     <div>
       <div id="mySidenav" class="sidenav">
-        <a href={"javascript:void(0)"} class="closebtn" onClick={closeNav}>
-          &times;
-        </a>
+        <a href="#title">About Me</a>
         <a href="#experience">Experience</a>
         <a href="#projects">Projects</a>
         <a href="javascript:void(0)">Contact</a>
       </div>
-      <span onClick={(e) => openNav(e)} class='button'>
+      <span onClick={navVisible ? closeNav : openNav} class='button'>
         <div class='bar1'></div>
         <div class='bar2'></div>
         <div class='bar3'></div>
